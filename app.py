aa0b9e910b5e2839533d7f41bf1f45e6e45d5428
Revanth3103 <68495515+Revanth3103@users.noreply.github.com>
2023-04-01 11:56:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,13 +33,10 @@ if df is not None:
     max_date = data['ds'].max()
     #st.write(max_date)
 
-"""
 
-Forecasts become less accurate with larger forecast days (1-365 days).
-"""
 
 periods_input = st.number_input('How many periods would you like to forecast into the future?',
-min_value = 1, max_value = 365)
+min_value = 1, max_value = 31)
 
 if df is not None:
     m = Prophet()
@@ -48,19 +45,17 @@ if df is not None:
 """
 ### Step 3: Visualize Forecast Data
 
-The below visual shows future predicted values. "yhat" is the predicted value, yhat_lower is min. value and yhat_upper is max. value we can use from obtained data.
+The below visual shows future predicted values. "yhat" is the predicted value.
 """
 if df is not None:
     future = m.make_future_dataframe(periods=periods_input)
     
     forecast = m.predict(future)
-    fcst = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+    fcst = forecast[['ds', 'yhat']]
 
     fcst_filtered =  fcst[fcst['ds'] > max_date]    
     st.write(fcst_filtered)
-    """
-    The next few visuals show a high level trend of predicted values, day of week trends, and yearly trends (if dataset covers multiple years).
-    """
+ 
     fig2 = m.plot_components(forecast)
     st.write(fig2)
     st.write('Trend is like similarity which is observed from given data and plot depends on datestamp') 
